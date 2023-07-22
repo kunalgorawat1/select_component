7c836e5d7dc9b940b6d53d3d38ad1d1e3fa69b7c
Kunal Gorawat <kunalgorawat@gmail.com>
2023-07-22 22:58:16 +0530

    Single Select addition

---
+++ src/Select.tsx
@@ -0,0 +1,56 @@
+import styles from "./select.module.css"
+import {useState} from 'react'
+type SelectOption = {
+    label: string,
+    value: any
+}
+
+type SelectProps = {
+    options: SelectOption[]
+    value?: SelectOption
+    onChange: (value: SelectOption | undefined) => void
+}
+export function Select({value, onChange, options}: SelectProps) {
+    const [isOpen, setIsOpen] = useState(true)
+    const clearOptions = () =>  {
+        onChange(undefined)
+    }
+
+    const selectOption = (option: SelectOption ) => {
+        onChange(option)
+    }
+
+    const isOptionSelected = (option: SelectOption) =>  {
+        return option === value 
+    }
+
+    return (
+        <>
+            <div 
+                onBlur={() => setIsOpen(false)}
+                onClick={() => setIsOpen(prev => !prev)} 
+                tabIndex={0} 
+                className={styles.container}>
+                <span className={styles.value}>{value?.label}</span>
+                <button onClick={ e => {
+                    e.stopPropagation()
+                    clearOptions()
+                }} className={styles["clear-btn"]}>&times;</button>
+                <div className={styles.divider}></div>
+                <div className={styles.caret}></div>
+                <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+                    {options.map(option => (
+                        <li onClick={e => {
+                            e.stopPropagation()
+                            selectOption(option)
+                            setIsOpen(false)
+                        }}
+                         key={option.label} 
+                         className={`${styles.option} ${isOptionSelected(option) ? styles.selected : ""}`}
+                         >{option.label}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
